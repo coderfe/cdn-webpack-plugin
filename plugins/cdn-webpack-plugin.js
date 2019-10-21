3f71f479ba70ad28d449a128919c37a8362ddd62
coderfee <coderfee@outlook.com>
2019-10-21 18:20:19 +0800

    Update

--- plugins/cdn-webpack-plugin.js
+++ plugins/cdn-webpack-plugin.js
@@ -1,36 +1,34 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const pluginName = "CdnWebpackPlugin";
 
 class CdnWebpackPlugin {
   constructor(cdn) {
     if (!cdn) {
-      throw Error('cdn is required');
+      throw Error("cdn is required");
     }
     this.cdn = cdn;
   }
 
   apply(compiler) {
     compiler.hooks.compilation.tap(pluginName, compilation => {
-      HtmlWebpackPlugin.getHooks(compilation).alterAssetTags.tapAsync(
-        pluginName,
-        (htmlWebpackPluginData, cb) => {
-          const assetsTags = htmlWebpackPluginData.assetTags.styles.concat(htmlWebpackPluginData.assetTags.scripts);
-          Promise.all(assetsTags.map(assetTag => this.promisify(compilation.assets, assetTag)))
-            .then(res => {
-              console.log(res);
-              cb(null, htmlWebpackPluginData);
-            }).catch(cb);
-        }
-      );
+      HtmlWebpackPlugin.getHooks(compilation).alterAssetTags.tapAsync(pluginName, (htmlWebpackPluginData, cb) => {
+        const assetsTags = [...htmlWebpackPluginData.assetTags.styles, ...htmlWebpackPluginData.assetTags.scripts];
+        Promise.all(assetsTags.map(assetTag => this.promisify(compilation.assets, assetTag)))
+          .then(res => {
+            console.log(res);
+            cb(null, htmlWebpackPluginData);
+          })
+          .catch(cb);
+      });
     });
   }
 
   promisify(assets, assetTag) {
-    const attr = assetTag.tagName === 'link' ? 'href' : 'src';
+    const attr = assetTag.tagName === "link" ? "href" : "src";
     const filename = assetTag.attributes[attr];
     return this.upload(filename, assets[filename].source()).then(url => {
       assetTag.attributes[attr] = url;
@@ -40,9 +38,9 @@ class CdnWebpackPlugin {
 
   upload(filename, content) {
     return fetch(this.cdn, {
-      method: 'post',
-      body: JSON.stringify({filename, content}),
-      headers: {'Content-Type': 'application/json'},
+      method: "post",
+      body: JSON.stringify({ filename, content }),
+      headers: { "Content-Type": "application/json" }
     }).then(res => res.text());
   }
 }
